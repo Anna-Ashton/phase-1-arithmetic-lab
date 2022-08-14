960734d2ed6c9acbd675155c4ab714dfec16b058
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-14 23:05:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1=2;
+const num2=31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random(0));
+
+const num3=40;
+const num4=10;
+const mod = num3 / num4;
+
+
+const max= Math.max(7,8,12,17,20);
